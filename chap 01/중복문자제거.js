6810b9668df4ba2782ef7dda77e2e5b8d7bb4d5c
KoreanCodingMachine <59787529+KoreanCodingMachine@users.noreply.github.com>
2022-04-24 00:07:21 +0900

    commit algorithm

--- chap 01/중복문자제거.js
+++ chap 01/중복문자제거.js
@@ -13,8 +13,29 @@
 // ▣ 출력예제 1
 // kset
 
-function solution(s) {
-  let answer;
+//string.indexOf(searchvalue,position)
+//첫번째 해당하는 인덱스만 반환한다. 없으면 -1을 반환한다.
+//position속성을 이용해 시작 인덱스를 정할수 있다.
 
-  return answer;
+// function solution(s) {
+//   let answer = '';
+//   for (let i = 0; i < s.length; i++) {
+//     console.log(s[i], i, s.indexOf(s[i]));
+//     if (s.indexOf(s[i]) === i) answer += s[i];
+//   }
+//   return answer;
+// }
+
+// console.log(solution('ksekkset'));
+
+// 특정문자 찾기
+// k의 개수는?
+
+function solution(s) {
+  let answer = 0;
+  let pos = s.indexOf('k');
+  while (pos !== -1) {
+    answer++;
+    pos = s.indexOf('k', pos + 1);
+  }
 }
